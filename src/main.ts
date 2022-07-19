4f63ba6a56a90566562596c3151be97f72d370be
Tom Haile <bradford.haile@gmail.com>
2022-07-19 12:15:44 -0500

    upgrade lib versions

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
-import * as core from '@actions/core'
-const {GitHub, context} = require('@actions/github')
+const core = require('@actions/core')
+const github = require('@actions/github')
 const parse = require('parse-diff')
 
 async function run() {
   try {
-    const token = core.getInput('github-token', {required: true})
-    const github = new GitHub(token, {})
     const keyword = core.getInput('keyword')
     const message = core.getInput('message')
 
-    const diff_url = context.payload.pull_request.diff_url
+    const diff_url = github.context.payload.pull_request.diff_url
     const result = await github.request(diff_url)
     const files = parse(result.data)
     core.exportVariable('files', files)
